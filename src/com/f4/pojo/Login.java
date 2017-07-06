7e1106bd08a5aaafc01ee97a25e8befb5547ebdb
YangXuLei <yangxulei84@qq.com>
2017-07-06 19:47:49 +0800

    add Login get set

--- src/com/f4/pojo/Login.java
+++ src/com/f4/pojo/Login.java
@@ -4,4 +4,33 @@ package com.f4.pojo;
  * Created by yangxulei on 2017/7/6.
  */
 public class Login {
+    int id;
+    String uname;
+    String upwd;
+    String role;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUname() {
+        return uname;
+    }
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+    public String getUpwd() {
+        return upwd;
+    }
+    public void setUpwd(String upwd) {
+        this.upwd = upwd;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
