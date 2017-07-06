601a5fda478e0d896842339a2c887e44149bb803
YangXuLei <yangxulei84@qq.com>
2017-07-06 19:51:49 +0800

    add TeacherInfo get set

--- src/com/f4/pojo/TeacherInfo.java
+++ src/com/f4/pojo/TeacherInfo.java
@@ -4,4 +4,55 @@ package com.f4.pojo;
  * Created by yangxulei on 2017/7/6.
  */
 public class TeacherInfo {
+    String  rname;
+    String  email;
+    String  job;
+    String  department;
+    String  telephone;
+    String  sid;
+    String  sex;
+    public String getRname() {
+        return rname;
+    }
+    public void setRname(String rname) {
+        this.rname = rname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getJob() {
+        return job;
+    }
+    public void setJob(String job) {
+        this.job = job;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public String getTelephone() {
+        return telephone;
+    }
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    public String getSid() {
+        return sid;
+    }
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+    public String getSex() {
+        return sex;
+    }
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+
 }
