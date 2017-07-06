8fdbeb3e31dd60269e2ec6b049289a2ace315761
YangXuLei <yangxulei84@qq.com>
2017-07-06 19:46:56 +0800

    add UserInfo get set

--- src/com/f4/pojo/UserInfo.java
+++ src/com/f4/pojo/UserInfo.java
@@ -4,4 +4,67 @@ package com.f4.pojo;
  * Created by yangxulei on 2017/7/6.
  */
 public class UserInfo {
+    int id;
+    String uname;
+    String newuname;
+    String runame;
+    String usex;
+    String birthday;
+    String photo;
+    String address;
+    String resume;
+    public String getNewuname() {
+        return newuname;
+    }
+    public void setNewuname(String newuname) {
+        this.newuname = newuname;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUname() {
+        return uname;
+    }
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+    public String getRuname() {
+        return runame;
+    }
+    public void setRuname(String runame) {
+        this.runame = runame;
+    }
+    public String getUsex() {
+        return usex;
+    }
+    public void setUsex(String usex) {
+        this.usex = usex;
+    }
+    public String getBirthday() {
+        return birthday;
+    }
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+    public String getPhoto() {
+        return photo;
+    }
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getResume() {
+        return resume;
+    }
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
 }
