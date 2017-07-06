fb02125b307cd322fc2412b0d8f977f49a219125
YangXuLei <yangxulei84@qq.com>
2017-07-06 19:52:39 +0800

    add TeacherRelation get set

--- src/com/f4/pojo/TeacherRelation.java
+++ src/com/f4/pojo/TeacherRelation.java
@@ -4,4 +4,47 @@ package com.f4.pojo;
  * Created by yangxulei on 2017/7/6.
  */
 public class TeacherRelation {
+    String tid;
+    String sid;
+    String cid;
+    String tname;
+    String cname;
+    String sname;
+    public String getTid() {
+        return tid;
+    }
+    public void setTid(String tid) {
+        this.tid = tid;
+    }
+    public String getSid() {
+        return sid;
+    }
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+    public String getCid() {
+        return cid;
+    }
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+    public String getTname() {
+        return tname;
+    }
+    public void setTname(String tname) {
+        this.tname = tname;
+    }
+    public String getCname() {
+        return cname;
+    }
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+    public String getSname() {
+        return sname;
+    }
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
 }
