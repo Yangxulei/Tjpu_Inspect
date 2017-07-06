e8be8c04e889e7fdafa8c660802b1c406e7cf4f9
shuo <719250465@qq.com>
2017-07-06 19:55:12 +0800

    新加了FindEduction findLogin action

--- src/com/f4/action/FindEductionalInfo.java
+++ src/com/f4/action/FindEductionalInfo.java
@@ -3,5 +3,74 @@ package com.f4.action;
 /**
  * Created by 任永硕 on 2017/7/6.
  */
-public class FindEductionalInfo {
+
+
+        import java.io.IOException;
+        import java.io.PrintWriter;
+        import java.util.HashMap;
+        import java.util.Map;
+
+        import org.apache.struts2.ServletActionContext;
+        import org.apache.struts2.convention.annotation.Action;
+        import org.apache.struts2.convention.annotation.Namespace;
+        import org.apache.struts2.convention.annotation.ParentPackage;
+
+        import com.alibaba.fastjson.JSON;
+        import com.f4.dao.DBUtils;
+        import com.opensymphony.xwork2.ActionSupport;
+        @ParentPackage("struts-default")
+        @Namespace("/")
+public class FindEducationalInfo extends ActionSupport{
+    Map map;
+    int page;
+    int rows;
+    String classname="";
+    String subject="";
+    public String getClassname() {
+        return classname;
+    }
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    @Action("findEducationalInfo")
+    public void findEducationalInfo(){
+        DBUtils db=new DBUtils();
+        map=new HashMap();
+        map.put("total", db.getEducationalInfoSize());
+        map.put("rows", db.findEducationalInfo(page, rows,classname,subject));
+        String json=JSON.toJSONString(map);
+        PrintWriter out;
+        try {
+            ServletActionContext.getResponse().setCharacterEncoding("utf-8");
+            out=ServletActionContext.getResponse().getWriter();
+            out.print(json);
+            out.flush();
+            out.close();
+            db.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public int getPage() {
+        return page;
+    }
+    public void setPage(int page) {
+        this.page = page;
+    }
+    public int getRows() {
+        return rows;
+    }
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
 }
--- src/com/f4/action/findLogin.java
+++ src/com/f4/action/findLogin.java
@@ -3,5 +3,72 @@ package com.f4.action;
 /**
  * Created by 任永硕 on 2017/7/6.
  */
-public class findLogin {
+
+
+        import java.io.IOException;
+        import java.io.PrintWriter;
+        import java.util.HashMap;
+        import java.util.Map;
+
+        import org.apache.struts2.ServletActionContext;
+        import org.apache.struts2.convention.annotation.Action;
+        import org.apache.struts2.convention.annotation.Namespace;
+        import org.apache.struts2.convention.annotation.ParentPackage;
+
+        import com.alibaba.fastjson.JSON;
+        import com.f4.dao.DBUtils;
+        import com.opensymphony.xwork2.ActionSupport;
+@ParentPackage("struts-default")
+@Namespace("/")
+public class findLogin extends ActionSupport {
+
+    Map map;
+    int page;
+    int rows;
+    public Map getMap() {
+        return map;
+    }
+    public void setMap(Map map) {
+        this.map = map;
+    }
+    String uname="";
+    public String getUname() {
+        return uname;
+    }
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+    @Action("findLogin")
+    public void findLogin(){
+        DBUtils db=new DBUtils();
+        map=new HashMap();
+        map.put("total", db.getUserInfoSize());
+        map.put("rows", db.findLogin(page, rows,uname));
+        String  json=JSON.toJSONString(map);
+        PrintWriter out;
+        try {
+            ServletActionContext.getResponse().setCharacterEncoding("utf-8");
+            out=ServletActionContext.getResponse().getWriter();
+            out.print(json);
+            out.flush();
+            out.close();
+            db.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public int getPage() {
+        return page;
+    }
+    public void setPage(int page) {
+        this.page = page;
+    }
+    public int getRows() {
+        return rows;
+    }
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
 }
