d3771a7cc27b2afb7c6a2f4a9c34f0dc7c6bbbd2
YangXuLei <yangxulei84@qq.com>
2017-07-06 19:56:36 +0800

    add xunchaInfo  get set

--- src/com/f4/pojo/xunchaInfo.java
+++ src/com/f4/pojo/xunchaInfo.java
@@ -3,5 +3,143 @@ package com.f4.pojo;
 /**
  * Created by yangxulei on 2017/7/6.
  */
+//用中文拼音简单易懂。。。。。。。。😁哈哈哈
 public class xunchaInfo {
+    String xunchayuan;
+    String searchdate;
+    String zhouci;
+    String jieci;
+    String grade;
+    String classname;
+    String tname;
+    String subject;
+    String ontime;
+    String tzhuangtai;
+    String absence;
+    String late;
+    String sleep;
+    String classdiscipline;
+    String playphone;
+    String chat;
+    String xueqi;
+    public String getXueqi() {
+        return xueqi;
+    }
+    public void setXueqi(String xueqi) {
+        this.xueqi = xueqi;
+    }
+
+
+    String fangjian;
+    String nianfen;
+    public String getNianfen() {
+        return nianfen;
+    }
+    public void setNianfen(String nianfen) {
+        this.nianfen = nianfen;
+    }
+    public String getFangjian() {
+        return fangjian;
+    }
+    public void setFangjian(String fangjian) {
+        this.fangjian = fangjian;
+    }
+
+
+    public String getPlayphone() {
+        return playphone;
+    }
+    public void setPlayphone(String playphone) {
+        this.playphone = playphone;
+    }
+    public String getChat() {
+        return chat;
+    }
+    public void setChat(String chat) {
+        this.chat = chat;
+    }
+    public String getXunchayuan() {
+        return xunchayuan;
+    }
+    public void setXunchayuan(String xunchayuan) {
+        this.xunchayuan = xunchayuan;
+    }
+    public String getSearchdate() {
+        return searchdate;
+    }
+    public void setSearchdate(String searchdate) {
+        this.searchdate = searchdate;
+    }
+    public String getZhouci() {
+        return zhouci;
+    }
+    public void setZhouci(String zhouci) {
+        this.zhouci = zhouci;
+    }
+    public String getJieci() {
+        return jieci;
+    }
+    public void setJieci(String jieci) {
+        this.jieci = jieci;
+    }
+    public String getGrade() {
+        return grade;
+    }
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+    public String getClassname() {
+        return classname;
+    }
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+    public String getTname() {
+        return tname;
+    }
+    public void setTname(String tname) {
+        this.tname = tname;
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    public String getOntime() {
+        return ontime;
+    }
+    public void setOntime(String ontime) {
+        this.ontime = ontime;
+    }
+    public String getTzhuangtai() {
+        return tzhuangtai;
+    }
+    public void setTzhuangtai(String tzhuangtai) {
+        this.tzhuangtai = tzhuangtai;
+    }
+    public String getAbsence() {
+        return absence;
+    }
+    public void setAbsence(String absence) {
+        this.absence = absence;
+    }
+    public String getLate() {
+        return late;
+    }
+    public void setLate(String late) {
+        this.late = late;
+    }
+    public String getSleep() {
+        return sleep;
+    }
+    public void setSleep(String sleep) {
+        this.sleep = sleep;
+    }
+    public String getClassdiscipline() {
+        return classdiscipline;
+    }
+    public void setClassdiscipline(String classdiscipline) {
+        this.classdiscipline = classdiscipline;
+    }
 }
